22ed4ad8c569dda5133097d4332590d2fc72a711
mpzhu <mpzhu@andrew.cmu.edu>
2012-09-05 18:13:37 -0400

    initial commit

--- hw0-mpzhu/src/main/java/DependencyExample.java
+++ hw0-mpzhu/src/main/java/DependencyExample.java
@@ -1,5 +1,3 @@
-
-
 import java.io.StringReader;
 import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.objectbank.TokenizerFactory;
@@ -7,25 +5,20 @@ import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
 import edu.stanford.nlp.process.Tokenizer;
 
 /**
-* An example for Homework 0 of 11791 F12
-*
-11 * @author Zi Yang <ziy@cs.cmu.edu>
-*/
- public class DependencyExample {
- /**
-* Tokenize a sentence in the argument, and print out
-* the tokens to the console.
-*
-* @param args
-* Set the first argument as the sentence to
-* be tokenized.
-*/
-public static void main(String[] args) {
-TokenizerFactory<Word> factory =
-PTBTokenizerFactory.newTokenizerFactory();
-Tokenizer<Word> tokenizer =
-factory.getTokenizer(new StringReader(args[0]));
-System.out.println(tokenizer.tokenize());
+ * An example for Homework 0 of 11791 F12
+ * 
+ * 11 * @author Zi Yang <ziy@cs.cmu.edu>
+ */
+public class DependencyExample {
+  /**
+   * Tokenize a sentence in the argument, and print out the tokens to the console.
+   * 
+   * @param args
+   *          Set the first argument as the sentence to be tokenized.
+   */
+  public static void main(String[] args) {
+    TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
+    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
+    System.out.println(tokenizer.tokenize());
+  }
 }
-}
-
